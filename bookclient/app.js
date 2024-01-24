8a183d972a9526113aa5e44e8debc961f0539c56
Xibanez <you@example.com>
2024-01-24 12:05:41 +0100

    Refactorización de funciones para poder hacer autorefresh al postear/updatear

--- bookclient/app.js
+++ bookclient/app.js
@@ -1,27 +1,28 @@
 'use strict';
 
-//const fetch = require('node-fetch');
-//import fetch from 'node-fetch'
-// const recurso = "http://127.0.0.1:8080";
-const URL = "https://eu-west-2.aws.data.mongodb-api.com/app/application-0-xugxs/endpoint/books";
+const booksURL = "https://eu-west-2.aws.data.mongodb-api.com/app/application-0-xugxs/endpoint/books";
 
 var notif = document.getElementById("message");
 var titext = document.getElementById("title-text");
 var autext = document.getElementById("author-text");
 var imgtext = document.getElementById("img-text");
 var submitbut = document.getElementById("submit-btt");
-//Get all the books:
-//TODO
-fetch(URL)
-    .then(res => res.json())
-    .then(json => inicio(json));
-
-
-
 //To know if we are updating
 var updateFlag = false;
 
-const inicio = (books => {
+//Execute when document is loaded
+document.addEventListener('load', refreshBooks(booksURL));
+
+//Get all the books:
+function refreshBooks(URL) {
+    fetch(URL)
+        .then(res => res.json())
+        .then(json => createBookslayout(json));
+}
+
+
+
+function createBookslayout(books) {
     let cadenaDOM = "";
     books.forEach(book => {
         cadenaDOM +=
@@ -65,55 +66,55 @@ const inicio = (books => {
             showNotification("Let's update " + elem.getAttribute("name"));
         })
     })
+}
+submitbut.addEventListener("click", function (event) {
+    event.preventDefault();
+    var data = {
+        title: titext.value,
+        author: autext.value,
+        img: imgtext.value
+    };
+    //fetch uses a request object
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    var request = {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.     
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit       
+        headers: {
+            "Content-Type": "application/json",
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    };
 
-    submitbut.addEventListener("click", function (event) {
-        event.preventDefault();
-        var data = {
-            title: titext.value,
-            author: autext.value,
-            img: imgtext.value
-        };
-        //fetch uses a request object
-        // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-        var request = {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.     
-            // mode: "cors", // no-cors, *cors, same-origin
-            // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-            // credentials: "same-origin", // include, *same-origin, omit       
-            headers: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            // redirect: "follow", // manual, *follow, error
-            // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        };
-
-        if (updateFlag) {
-            //TODO
-            // PUT request here, using input text values to build JSON
+    if (updateFlag) {
+        //TODO
+        // PUT request here, using input text values to build JSON
 
 
-            // Change Submit button text
-            changeButton();
-        } else {
-            //TODO
-            // Post request here, using input text values to build JSON
-            fetch(URL, request)
-                .then(res => {
-                    showNotification("Updating Data... " + titext.value);
-                    console.log("POST result: " + res)
-                    //TODO : shown books must be refreshed
-                })
-                .catch(err => {
-                    showNotification("ERROR Updating Data... ")
-                    console.log("POST error: " + err)
-                });
-            showNotification("Posting Data... " + titext.value)
-        }
-    })
-});
-
+        // Change Submit button text
+        changeButton();
+    } else {
+        //TODO
+        // Post request here, using input text values to build JSON
+        fetch(booksURL, request)
+            .then(res => {
+                showNotification("Updating Data... " + titext.value);
+                console.log("POST result: " + res)
+                //TODO : shown books must be refreshed
+            })
+            .catch(err => {
+                showNotification("ERROR Updating Data... ")
+                console.log("POST error: " + err)
+            });
+        showNotification("Posting Data... " + titext.value)
+    }
+    //Refresh books
+    refreshBooks(booksURL);
+})
 
 function changeButton() {
     if (updateFlag) {
